f2a8f5e29eebcf05bdc98e15f7ff6b7b5ff03766
Yumna Mubeen <yumnamubeen0019@gamil.com>
2025-07-25 16:13:38 +0500

    backend pino logger

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/connectDB'); 
+const connectDB = require('./config/connectDB');
+const pino = require('pino-http')(); 
 
-// Load environment variables
 dotenv.config();
 
-// Initialize Express app
 const app = express();
 
-// Connect to MongoDB
-connectDB(); // ⬅ correct usage with capital D
+connectDB();
 
-// Middleware
+app.use(pino);
+
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 
 // Test route
 app.get('/', (req, res) => {
-  res.send('✅ Backend is running');
+  res.send('Backend is running');
 });
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
